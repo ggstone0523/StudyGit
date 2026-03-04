a11453ada7cab3eacda81ba9a66ec18c86e4832a
Forharu40 <bugeun.sagong@gmail.com>
2026-03-04 10:22:56 +0900

    [Feat] Add minus feature
    Signed-off-by: Forharu40 <bugeun.sagong@gmail.com>

--- calculator.c
+++ calculator.c
@@ -14,6 +14,11 @@ double divide(int x, int y)
 	return x / y;
 }
 
+int minus(int x, int y)
+{
+    return x - y;
+}
+
 int main()
 {
     int c;
@@ -30,20 +35,29 @@ int main()
                 (void)scanf("%d %d", &x, &y);
                 printf("%d\n", plus(x, y));
                 break;
-	    case 3:
+
+            case 2:
+                printf("please enter x and y: ");
+                (void)scanf("%d %d", &x, &y);
+                printf("%d\n", minus(x, y));
+                break;
+
+            case 3:
                 printf("please enter x and y: ");
                 (void)scanf("%d %d", &x, &y);
                 printf("%d\n", multifly(x, y));
                 break;
-	    case 4:
-		printf("please enter x and y: ");
-		(void)scanf("%d %d", &x, &y);
-		if (y == 0) {
-			printf("can't divided by zero\n");
-			break;
-		}
-		printf("%.2\n", divide(x, y));
-		break;
+                
+            case 4:
+                printf("please enter x and y: ");
+                (void)scanf("%d %d", &x, &y);
+                if (y == 0) {
+                    printf("can't divided by zero\n");
+                    break;
+                }
+                printf("%.2\n", divide(x, y));
+                break;
+
             default:
                 continue;
         }
